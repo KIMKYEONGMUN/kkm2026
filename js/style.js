8fd6a78a346f0c28854b40e2ae0ea76aefcfab88
KIMKYEONGMUN <139826895+KIMKYEONGMUN@users.noreply.github.com>
2025-11-24 00:11:56 +0900

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,28 @@
+$(function(){
+    // Initialize Lenis
+    const lenis = new Lenis({
+    autoRaf: true,
+    });
+
+    // Listen for the scroll event and log the event data
+    lenis.on('scroll', (e) => {
+    console.log(e);
+    });
+
+    $('.sect3 .list li').click(function(){
+        $('.sect3 .list li').removeClass('on');
+        $(this).addClass('on');
+        $('.sect3 .item').hide();
+        $('.sect3 .item').eq($(this).index()).fadeIn(200);
+        $('html, body').animate({
+            scrollTop: $('.sect3').offset().top,
+        });
+    });
+
+    $('.btn_top').click(function(){
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 0);
+    });
+
+});// jquery end

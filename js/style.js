456c34ef0c9d7c374633fb33476d1235abe2cc6a
KIMKYEONGMUN <139826895+KIMKYEONGMUN@users.noreply.github.com>
2025-11-29 19:30:23 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -5,9 +5,9 @@ $(function(){
     });
 
     // Listen for the scroll event and log the event data
-    lenis.on('scroll', (e) => {
-    console.log(e);
-    });
+    // lenis.on('scroll', (e) => {
+    // console.log(e);
+    // });
 
     $('.sect3 .list li').click(function(){
         $('.sect3 .list li').removeClass('on');
